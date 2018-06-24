9486fb0e62cf28581000f48500743cd14f4aabe8
JeromyIrvine <jer@carbonated.io>
2018-06-24 19:16:09 -0400

    Fix to allow LHA to move towards the source even if its exhausted

--- src/role.linkHarvester.js
+++ src/role.linkHarvester.js
@@ -8,7 +8,7 @@ var roleLinkHarvester = {
         let link  = Game.getObjectById(creep.memory.linkId);
 
         let result = creep.harvest(source);
-        if (result == ERR_NOT_IN_RANGE) {
+        if (result == ERR_NOT_IN_RANGE || result == ERR_NOT_ENOUGH_ENERGY) {
             creep.moveTo(source);
         } else if (result == OK) {
             if (creep.transfer(link, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
